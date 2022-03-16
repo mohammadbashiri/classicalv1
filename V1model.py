5cc0d1985aaacf0f06e49505f25a540d1625c673
lucabaroni <loocabaroni@gmail.com>
2022-03-16 11:22:17 +0100

    update models

--- V1model.py
+++ V1model.py
@@ -1,3 +1,4 @@
+#%%
 import numpy as np
 from V1cells import *
 
@@ -11,12 +12,13 @@ class V1model:
         self,
         inputs_res,
         inputs_fov,
-        cell_density,
+        n_pos_per_deg,
         n_ori,
         n_phase,
         sf,
         sigma_x_values=[1],
         sigma_y_values=[1],
+        with_simple_cells=True,
         with_complex_cells=True,
         noisy_responses=False,
     ):
@@ -25,7 +27,7 @@ class V1model:
         Args:
             inputs_res (int, int): dimension of the inputs and of the cells filters in x and y dimension
             inputs_fov (float, float): field of view in degree of visual field covered by the input
-            cell_density (float): number of cells per degree of visual field
+            n_pos_per_deg (float): number of cells per degree of visual field
             n_ori (int): number of equidistant orientations of cell's Gabor filters
             n_phase (int): number of equidistant phases of cell's Gabor filters
             sf (list of floats): spatial frequencies values of cell's Gabor filters
@@ -39,7 +41,7 @@ class V1model:
         self.inputs_fov = inputs_fov
         self.xlim = (-inputs_fov[0] / 2, inputs_fov[0] / 2)
         self.ylim = (-inputs_fov[1] / 2, inputs_fov[1] / 2)
-        self.cell_density = cell_density
+        self.n_pos_per_deg = n_pos_per_deg
         self.n_ori = n_ori
         self.sf = sf
         if type(self.sf) != list:
@@ -50,8 +52,12 @@ class V1model:
         self.positions = self._get_positions()
         self.orientations = self._get_orientations()
         self.phases = self._get_phases()
+        self.with_simple_cells = with_simple_cells
         self.with_complex_cells = with_complex_cells
-        self.simple_cells = self.create_simple_cells()
+        if self.with_simple_cells:
+            self.simple_cells = self.create_simple_cells()
+        else:
+            self.simple_cells = []
         if self.with_complex_cells == True:
             self.complex_cells = self.create_complex_cells()
         else:
@@ -61,15 +67,15 @@ class V1model:
 
     def _get_positions(self):
         pos_x = (
-            np.linspace(-0.5, 0.5, int(self.cell_density * self.inputs_fov[0]))
+            np.linspace(-0.5, 0.5, int(self.n_pos_per_deg * self.inputs_fov[0]))
             * self.inputs_fov[0]
         )
         pos_y = (
-            np.linspace(-0.5, 0.5, int(self.cell_density * self.inputs_fov[1]))
+            np.linspace(-0.5, 0.5, int(self.n_pos_per_deg * self.inputs_fov[1]))
             * self.inputs_fov[1]
         )
-        pos_x = 1 / (self.cell_density) + pos_x[:-1]
-        pos_y = 1 / (self.cell_density) + pos_y[:-1]
+        pos_x = 1 / (self.n_pos_per_deg) + pos_x[:-1]
+        pos_y = 1 / (self.n_pos_per_deg) + pos_y[:-1]
         positions = [(x, y) for x in pos_x for y in pos_y]
         return positions
 
@@ -123,6 +129,157 @@ class V1model:
             for sigma_x in self.sigma_x_values
             for sigma_y in self.sigma_y_values
         ]
+
+    def get_responses(self, stim):
+        """returns cells responses to a given stimulus
+
+        Args:
+            stim ([2d numpy array]): stimulus
+
+        Returns:
+            [list of floats]: list containing cell's responses
+        """
+        resp = [cell.get_response(stim) for cell in self.cells]
+        if self.noisy_responses:
+            resp = [np.random.poisson(r) for r in resp]
+        return resp
+
+    def get_simple_cell_filters(self):
+        """function to get all the filters of simple cells as a 3d numpy array 
+
+        Returns:
+            [3d numpy array]: cell's filters
+        """
+        simple_cells_filters = np.array([cell.filter for cell in self.simple_cells])
+        return simple_cells_filters
+
+    def get_complex_cell_filters(self):
+        """function to get the filters and the 180 deg phase shifted filters of complex cells
+
+        Returns:
+            [list]: list containing 2 3d numpy arrays representing the cells
+                    filters and phase shifted filters
+        """
+        complex_cells_f1s = np.array([cell.f1 for cell in self.complex_cells])
+        complex_cells_f2s = np.array([cell.f2 for cell in self.complex_cells])
+        return complex_cells_f1s, complex_cells_f2s
+
+
+class V1model_random:
+    """
+    Classical V1 model with randomly extracted property values
+    """
+
+    def __init__(
+        self,
+        inputs_res,
+        inputs_fov,
+        n_cells=10000,
+        sf_values=[0.5, 0.7, 0.9, 1.1],
+        sigma=[0.2, 0.3, 0.4, 0.5],
+        gabor_aspect_ratio=[0.6, 0.8, 1],
+        with_complex_cells=True,
+        noisy_responses=False,
+        simple_complex_ratio=0.5,
+    ):
+
+        self.inputs_res = inputs_res
+        self.inputs_fov = inputs_fov
+        self.n_cells = n_cells
+        self.simple_complex_ratio = simple_complex_ratio
+        if with_complex_cells:
+            self.n_simple_cells = int(np.ceil(self.n_cells * simple_complex_ratio))
+            self.n_complex_cells = int(self.n_cells - self.n_simple_cells)
+        else:
+            self.n_simple_cells = self.n_cells
+            self.n_complex_cells = 0
+        self.xlim = (-inputs_fov[0] / 2, inputs_fov[0] / 2)
+        self.ylim = (-inputs_fov[1] / 2, inputs_fov[1] / 2)
+        self.sf = sf_values
+        if type(self.sf) != list:
+            self.sf = [self.sf]
+
+        self.sigma = sigma
+        if type(self.sigma) != list:
+            self.sigma = [self.sigma]
+        self.gabor_aspect_ratio = gabor_aspect_ratio
+
+        if type(self.gabor_aspect_ratio) != list:
+            self.gabor_aspect_ratio = [self.gabor_aspect_ratio]
+        self.noisy_responses = noisy_responses
+        self.with_complex_cells = with_complex_cells
+
+        self.simple_cells, self.simple_cell_props = self.create_simple_cells()
+        if self.with_complex_cells == True:
+            self.complex_cells, self.complex_cells_props = self.create_complex_cells()
+        else:
+            self.complex_cells = []
+        self.cells = self.simple_cells + self.complex_cells
+
+    def gen_properties(self, n, phase=True):
+        properties = {}
+        properties["pos"] = self.gen_positions(n)
+        properties["ori"] = np.random.uniform(0, np.pi, size=n)
+        if phase == True:
+            properties["phase"] = np.random.uniform(0, 2 * np.pi, size=n)
+        properties["sf"] = np.random.choice(self.sf, size=n)
+        properties["sigma_x"] = np.random.choice(self.sigma, size=n)
+        properties["sigma_y"] = properties["sigma_x"] / np.random.choice(
+            self.gabor_aspect_ratio, size=n
+        )
+        return properties
+
+    def gen_positions(self, n):
+        # pos_x = np.random.uniform(low=-0.5, high=0.5, size=n) * self.inputs_fov[0]
+        # pos_y = np.random.uniform(low=-0.5, high=0.5, size=n) * self.inputs_fov[1]
+        positions = np.random.uniform(low=-0.5, high=0.5, size=[n, 2])
+        positions[:, 0] = positions[:, 0] * self.inputs_fov[0]
+        positions[:, 1] = positions[:, 1] * self.inputs_fov[1]
+        # positions = [(x, y) for x in pos_x for y in pos_y]
+        return positions
+
+    def create_simple_cells(self):
+        print("creating " + str(self.n_simple_cells) + " simple cells")
+        props = self.gen_properties(self.n_simple_cells, phase=True)
+        cells = [
+            simple_cell(
+                self.inputs_res,
+                self.xlim,
+                self.ylim,
+                pos,
+                theta,
+                sf,
+                phase,
+                sigmax,
+                sigmay,
+            )
+            for pos, theta, sf, phase, sigmax, sigmay in zip(
+                props["pos"],
+                props["ori"],
+                props["sf"],
+                props["phase"],
+                props["sigma_x"],
+                props["sigma_y"],
+            )
+        ]
+        return cells, props
+
+    def create_complex_cells(self):
+        print("creating " + str(self.n_complex_cells) + " complex cells")
+        props = self.gen_properties(self.n_complex_cells, phase=False)
+        cells = [
+            complex_cell(
+                self.inputs_res, self.xlim, self.ylim, pos, theta, sf, sigmax, sigmay,
+            )
+            for pos, theta, sf, sigmax, sigmay in zip(
+                props["pos"],
+                props["ori"],
+                props["sf"],
+                props["sigma_x"],
+                props["sigma_y"],
+            )
+        ]
+        return cells, props
 
     def get_responses(self, stim):
         """returns cells responses to a given stimulus
--- torchV1model.py
+++ torchV1model.py
@@ -1,47 +1,57 @@
-from V1model import V1model
+from math import gamma
+from V1model import V1model, V1model_random
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
-class classicalv1(nn.Module):
+class classicalV1(nn.Module):
     def __init__(
         self,
         inputs_res=(64, 64),
         inputs_fov=(8, 8),
-        cell_density=2,
+        n_pos_per_deg=2,
         n_ori=8,
         n_phase=4,
         sf=[0.5, 1, 2],
         sigma_x_values=[0.6],
         sigma_y_values=[0.6],
+        with_simple_cells=True,
         with_complex_cells=True,
+        noisy_responses=False,
     ):
         super().__init__()
         self.model = V1model(
             inputs_res,
             inputs_fov,
-            cell_density,
+            n_pos_per_deg,
             n_ori,
             n_phase,
             sf,
             sigma_x_values,
             sigma_y_values,
+            with_simple_cells,
             with_complex_cells,
+            noisy_responses,
         )
-        self.simple_cell_filters = nn.Parameter(
-            torch.Tensor(self.model.get_simple_cell_filters()), requires_grad=False
-        )
-        (f1, f2) = self.model.get_complex_cell_filters()
-        self.complex_cell_f1s = nn.Parameter(torch.Tensor(f1), requires_grad=False)
-        self.complex_cell_f2s = nn.Parameter(torch.Tensor(f2), requires_grad=False)
-        self.params = nn.ParameterDict(
-            {
-                "simple_cells": self.simple_cell_filters,
-                "complex_cells_f1": self.complex_cell_f1s,
-                "complex_cells_f2": self.complex_cell_f2s,
-            }
-        )
+        params = {}
+        if self.model.with_simple_cells == True:
+            self.simple_cell_filters = nn.Parameter(
+                torch.Tensor(self.model.get_simple_cell_filters()), requires_grad=False
+            )
+            params.update({"simple_cells": self.simple_cell_filters})
+        if self.model.with_complex_cells == True:
+            (f1, f2) = self.model.get_complex_cell_filters()
+            self.complex_cell_f1s = nn.Parameter(torch.Tensor(f1), requires_grad=False)
+            self.complex_cell_f2s = nn.Parameter(torch.Tensor(f2), requires_grad=False)
+            params.update(
+                {
+                    "complex_cells_f1": self.complex_cell_f1s,
+                    "complex_cells_f2": self.complex_cell_f2s,
+                }
+            )
+        self.params = nn.ParameterDict(params)
+        self.eval()
 
     def simple_cells_forward(self, x):
         x = torch.tensordot(x, self.simple_cell_filters, dims=[[1, 2], [1, 2]])
@@ -55,7 +65,73 @@ class classicalv1(nn.Module):
         return xc
 
     def forward(self, x):
-        xs = self.simple_cells_forward(x)
-        xc = self.complex_cells_forward(x)
-        resp = torch.cat((xs, xc), 1)
+        resp = []
+        if self.model.with_simple_cells == True:
+            resp.append(self.simple_cells_forward(x))
+        if self.model.with_complex_cells == True:
+            resp.append(self.complex_cells_forward(x))
+        resp = torch.cat(resp, 1)
+        return resp
+
+
+class classicalV1random(nn.Module):
+    def __init__(
+        self,
+        inputs_res=(64, 64),
+        inputs_fov=(11, 11),
+        n_cells=10000,
+        sf_values=[0.5, 0.7, 0.9, 1.1],
+        sigma=[0.2, 0.3, 0.4, 0.5],
+        gabor_aspect_ratio=[0.4, 0.6, 0.8],
+        with_complex_cells=True,
+        noisy_responses=False,
+        simple_complex_ratio=0.5,
+    ):
+        super().__init__()
+        self.model = V1model_random(
+            inputs_res=inputs_res,
+            inputs_fov=inputs_fov,
+            n_cells=n_cells,
+            sf_values=sf_values,
+            sigma=sigma,
+            gabor_aspect_ratio=gabor_aspect_ratio,
+            with_complex_cells=with_complex_cells,
+            noisy_responses=noisy_responses,
+            simple_complex_ratio=simple_complex_ratio,
+        )
+        self.simple_cell_filters = nn.Parameter(
+            torch.Tensor(self.model.get_simple_cell_filters()), requires_grad=False
+        )
+        params = {"simple_cells": self.simple_cell_filters}
+        if self.model.with_complex_cells == True:
+            (f1, f2) = self.model.get_complex_cell_filters()
+            self.complex_cell_f1s = nn.Parameter(torch.Tensor(f1), requires_grad=False)
+            self.complex_cell_f2s = nn.Parameter(torch.Tensor(f2), requires_grad=False)
+            params.update(
+                {
+                    "complex_cells_f1": self.complex_cell_f1s,
+                    "complex_cells_f2": self.complex_cell_f2s,
+                }
+            )
+        self.params = nn.ParameterDict(params)
+
+    def simple_cells_forward(self, x):
+        x = torch.tensordot(x, self.simple_cell_filters, dims=[[1, 2], [1, 2]])
+        x = F.relu(x)
+        return x
+
+    def complex_cells_forward(self, x):
+        x1 = torch.tensordot(x, self.complex_cell_f1s, dims=[[1, 2], [1, 2]])
+        x2 = torch.tensordot(x, self.complex_cell_f2s, dims=[[1, 2], [1, 2]])
+        xc = torch.sqrt(x1 ** 2 + x2 ** 2)
+        return xc
+
+    def forward(self, x):
+        with torch.no_grad():
+            if self.model.with_complex_cells == True:
+                xs = self.simple_cells_forward(x)
+                xc = self.complex_cells_forward(x)
+                resp = torch.cat((xs, xc), 1)
+            else:
+                resp = self.simple_cells_forward(x)
         return resp
