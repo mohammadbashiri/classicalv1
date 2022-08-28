97e6511a2c8f8eb812c22e437043dc88acd646a6
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2022-08-28 18:41:10 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="classicalv1",
+    version="0.0",
+    author="Luca Baroni",
+    author_email="",
+    description="",
+    url="https://github.com/lucabaroni/classicalv1",
+    packages=find_packages(),
+)
